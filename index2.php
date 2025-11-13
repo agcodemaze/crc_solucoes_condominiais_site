378156ce9fe6e8a5ed32ae55a383180c5a67d1e3
Codemaze <suporte@codemaze.com.br>
2025-11-13 19:32:50 -0300

    deploy

--- index2.php
+++ index2.php
@@ -223,7 +223,7 @@ de
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="100">
             <div class="service-item  position-relative">
               <div class="icon">
-                <i class="bi bi-activity"></i>
+                <i class="bi bi-award"></i>
               </div>
               <a href="#" class="stretched-link">
                 <h3>Formação e Experiência</h3>
@@ -237,7 +237,7 @@ de
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
             <div class="service-item position-relative">
               <div class="icon">
-                <i class="bi bi-cart-dash"></i>
+                <i class="bi bi-bar-chart"></i>
               </div>
               <a href="#" class="stretched-link">
                 <h3>Gestão Transparente</h3>
@@ -250,7 +250,7 @@ de
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="200">
             <div class="service-item position-relative">
               <div class="icon">
-                <i class="bi bi-building"></i>
+                <i class="bi bi-gear"></i>
               </div>
               <a href="#" class="stretched-link">
                 <h3>Atuação Técnica</h3>
@@ -264,7 +264,7 @@ de
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="400">
             <div class="service-item position-relative">
               <div class="icon">
-                <i class="bi bi-list-task"></i>
+                <i class="bi bi-people"></i>
               </div>
               <a href="#" class="stretched-link">
                 <h3>Proximidade com Condôminos</h3>
@@ -279,7 +279,7 @@ de
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="500">
             <div class="service-item position-relative">
               <div class="icon">
-                <i class="bi bi-calendar4-week"></i>
+                <i class="bi bi-graph-up-arrow"></i>
               </div>
               <a href="#" class="stretched-link">
                 <h3>Valorização Patrimonial</h3>
@@ -293,7 +293,7 @@ de
           <div class="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay="600">
             <div class="service-item position-relative">
               <div class="icon">
-                <i class="bi bi-speedometer"></i>
+                <i class="bi bi-balance-scale"></i>
               </div>
               <a href="#" class="stretched-link">
                 <h3>Segurança Jurídica</h3>
