54c0d4044cb5888c1e163ad3de8c2013dc9a61f2
Codemaze <suporte@codemaze.com.br>
2025-11-13 19:21:26 -0300

    deploy

--- index2.php
+++ index2.php
@@ -170,7 +170,10 @@ de
  segurança, economia e valorização patrimonial.</span></li>
         </ul>
 
-
+<div style="margin-top:20px; padding-left:6px; border-left:3px solid #0d6efd; font-style:italic; line-height:1.4;">
+  <strong style="font-size:1.1rem; font-weight:600;">Clériston Ramon Cruz</strong><br>
+  <span style="font-size:0.95rem; color:#555;">Diretor Executivo • Síndico Profissional</span>
+</div>
  
  
 
